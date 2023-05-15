9fa8810b6a47a13e585a7d2b023928a0610a1a7b
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2023-05-15 18:52:47 +0545

    add money and send money working

--- lib/Appbar functions/send_money.dart
+++ lib/Appbar functions/send_money.dart
@@ -10,6 +10,69 @@ class SendMoney extends StatefulWidget {
 class _SendMoneyState extends State<SendMoney> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(padding: const EdgeInsets.all(4),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  "Send Money",
+                  style: TextStyle(
+                      color: Colors.purple.shade900,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  "Available Balance Rs 882567.68",
+                  style: TextStyle(
+                      fontSize: 16,
+                      //fontWeight: FontWeight.bold,
+                      color: Colors.grey.shade800),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+               ListTile(
+                leading:  Icon(Icons.send_to_mobile_rounded,color: Colors.purple.shade900,),
+                title: const Text("Send to Khalti User",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 20),),
+                subtitle: const Text("Transfer money to Khalti Friends"),
+                trailing: Icon(Icons.arrow_forward_ios,color: Colors.purple.shade900,),
+                
+              ),
+               ListTile(
+                leading:  Icon(Icons.assured_workload_rounded,color: Colors.purple.shade900,),
+                title: const Text("Send to any Bank Account ",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 20),),
+                subtitle: const Text("Transfer money to banks in Nepal"),
+                trailing:  Icon(Icons.arrow_forward_ios,color: Colors.purple.shade900,),
+                
+              ),
+              ListTile(
+                leading:  Icon(Icons.corporate_fare_sharp,color: Colors.purple.shade900,),
+                title: const Text("Send to any Bank Account ",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 20),),
+                subtitle: const Text("Transfer money to Sahakari banks"),
+                trailing:  Icon(Icons.arrow_forward_ios,color: Colors.purple.shade900,),
+                
+              ),
+              
+
+
+
+          ],
+        ),),
+      ),
+    );
   }
 }
--- lib/Pages/support.dart
+++ lib/Pages/support.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:khalti_clone_ui/Pages/home.dart';
 
 class Support extends StatefulWidget {
   const Support({super.key});
@@ -11,10 +12,56 @@ class _SupportState extends State<Support> {
   @override
   Widget build(BuildContext context) {
      return Scaffold(
-      appBar: AppBar(title: const Text('Support'),),
-      body: const Center(
-        child: Text('Support Screen',style: TextStyle(fontSize: 40),)
+      //appBar: AppBar(title: const Text('Support'),),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const HomePage();
+              }));
+            },
+            child: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            )),
+        elevation: 0,
       ),
+      body: Padding(padding: const EdgeInsets.all(4),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Text(
+                "Help & Support",
+                style: TextStyle(
+                    color: Colors.purple.shade900,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Text(
+                "Customer Support,Feedbacks,FAQs",
+                style: TextStyle(
+                    fontSize: 16,
+                    //fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade800),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+        ],
+      ),)
     );
   }
 }
